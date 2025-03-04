c3ff81211f6a3329b94ade58a96d42ac0c28273c
Olumide Ilesanmi <ilesanmiolumide10@gmail.com>
2025-03-04 08:25:10 +0100

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -30,7 +30,7 @@ const page = () => {
 
   return (
     <div className='max-w-[450px]'>
-      <div className='w-[100%] h-[100vh] relative'>
+      <div className='w-full h-[100vh] relative'>
         {/* Image */}
         <Image 
           src='/images/pic1.jpeg' 
@@ -39,24 +39,36 @@ const page = () => {
           className='object-cover'
         />
         
-        <div className='flex items-center gap-2 absolute bottom-0 w-full m-3'>
-        <div className='w-[30%]'>
-          <Image src='/images/pic2.jpeg' alt='s' height={100} width={70} className='w-[100%] h-[120px] object-contain'/>
-        </div>
-        {/* Glassmorphic Transparent Overlay */}
-        <div className='w-[62%] h-[120px] rounded-lg rounded-tr-[0px] p-2 leading-[20px] text-white bg-[rgba(0,0,0,0.55)]  backdrop-filter'>
-          <div className='flex items-center gap-2 p-2 rounded-t-lg absolute text-sm right-[15px] top-[-3.1rem] font-[900] m-3 bg-[rgba(0,0,0,0.8)]  backdrop-filter'>
-            <Image src='/images/gps.jpg' alt='s' height={30} width={30} className='object-contain'/>
-            <h3>GPS Map Camera</h3>
+        <div className='flex items-center gap-2 absolute bottom-0 w-[96%] h-[100px] p-[1rem] left-[.5rem] right-2'>
+          {/* Left Image */}
+          <div className='w-[20%] h-full'>
+            <Image 
+              src='/images/pic2.jpeg' 
+              alt='s' 
+              height={100} 
+              width={70} 
+              className='w-full h-[100px] object-contain' 
+            />
           </div>
-          <p className='text-[1.1rem] font-[900]'>{data.location}</p>
-          <span className='text-[0.8rem]'>{data.address}</span>
-          <p className='text-[0.8rem]'>Lat {data.lat} Long {data.long}</p>
-          <p className='uppercase text-[0.8rem]'>{currentTime} GMT +1:00</p>
-        </div>
 
+          {/* Glassmorphic Transparent Overlay */}
+          <div className='w-[80%] h-[88px] rounded-md rounded-tr-[0px] padding leading-[20px] text-white bg-[rgba(0,0,0,0.55)] backdrop-filter'>
+            <div className='flex items-center gap-2 p-2 rounded-t-md absolute text-[0.65rem] right-[0] top-[-1rem] font-[900] m-3 bg-[rgba(0,0,0,0.8)] backdrop-filter'>
+              <Image 
+                src='/images/gps.jpg' 
+                alt='s' 
+                height={30} 
+                width={30} 
+                className='object-contain' 
+              />
+              <h3>GPS Map Camera</h3>
+            </div>
+            <p className='text-[1rem] font-[900]'>{data.location}</p>
+            <span className='text-[0.8rem]'>{data.address}</span>
+            <p className='text-[0.8rem]'>Lat {data.lat} Long {data.long}</p>
+            <p className='uppercase text-[0.8rem]'>{currentTime} GMT +1:00</p>
+          </div>
         </div>
-
       </div>
     </div>
   )
